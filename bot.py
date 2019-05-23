bc96d6b29400b48e99f2888889b6ec10738f35b5
Михаил Кренгауз <mihailkrengauz@MacBook-Pro-Mihail-Krengauz.local>
2019-05-23 17:01:40 +0300

    added eye filter

--- bot.py
+++ bot.py
@@ -3,7 +3,12 @@ import telebot
 from telebot import types
 from telebot.types import Message
 import cv2
-import invert
+from invert import apply_invert
+import run_face_swap
+from run_face_swap import face_swap
+import requests
+import Eyes
+from Eyes import big_eyes
 
 TOKEN = '895248757:AAH7tjGZjkiRSyAOCInlSn4F0auuaqD55XE'
 bot = telebot.TeleBot(TOKEN)
@@ -36,16 +41,16 @@ def start_invert(message):
 def send_invert(message):
     if bot.message_handler(content_types=['photo']):
         try:
-            file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
-            # downloaded_file = bot.download_file(file_info.file_path)
+            file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
 
-            src = file_info.file_path;
-            # with open(src, 'wb') as new_photo:
-            photo = cv2.imread(src)
-            inverted_photo1 = cv2.imread(cv2.bitwise_not(photo))
-            inverted_photo2 = cv2.imwrite("inverted_photo1", inverted_photo1)
-            # inverted_photo = apply_invert(new_photo)
-            bot.send_photo(message.chat.id, inverted_photo1)
+            with open("photos/inving" + str(message.message_id) + ".jpg", 'wb') as new_file:
+                new_file.write(downloaded_file)
+            apply_invert("photos/inving" + str(message.message_id) + ".jpg", message)
+            result = "photos/inved" + str(message.message_id) + ".jpg"
+            bot.send_photo(message.chat.id, (open(result, "rb")))
+            os.remove("photos/inving" + str(message.message_id) + ".jpg")
+            os.remove(result)
         except Exception as e:
             bot.send_message(message.chat.id, e)
     else:
@@ -55,24 +60,46 @@ def send_invert(message):
 @bot.message_handler(content_types=['photo'])
 def handle_face_swap(message):
     try:
-        file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
+        file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
 
-        src = file_info.file_path;
-        with open(src, 'wb') as new_file:
+        with open("photos/swapping" + str(message.message_id) + ".jpg", 'wb') as new_file:
             new_file.write(downloaded_file)
-        bot.send_message(message.chat.id, "Фото добавлено")
+        face_swap("photos/swapping" + str(message.message_id) + ".jpg", message)
+        result = "photos/swapped" + str(message.message_id) + ".jpg"
+        bot.send_photo(message.chat.id, (open(result, "rb")))
+        os.remove("photos/swapping" + str(message.message_id) + ".jpg")
+        os.remove(result)
     except Exception as e:
         bot.send_message(message.chat.id, e)
 
+    #     file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
+    #     downloaded_file = bot.download_file(file_info.file_path)
+    #
+    #     with open("photos/new_file.jpg", 'wb') as new_file:
+    #         new_file.write(downloaded_file)
+    #     bot.send_message(message.chat.id, "Фото добавлено")
+    # except Exception as e:
+    #     bot.send_message(message.chat.id, e)
+@bot.message_handler(content_types=['photo'])
+def handle_large_eyes(message):
+    try:
+        file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        with open("photos/eyes" + str(message.message_id) + ".jpg", 'wb') as new_file:
+            new_file.write(downloaded_file)
+        big_eyes("photos/eyes" + str(message.message_id) + ".jpg", message)
+        result = "photos/eyes" + str(message.message_id) + ".jpg"
+        bot.send_photo(message.chat.id, (open(result, "rb")))
+        os.remove("photos/eyes" + str(message.message_id) + ".jpg")
+        os.remove(result)
+    except Exception as e:
+        bot.send_message(message.chat.id, e)
 
 @bot.message_handler(func=lambda message: True)
 def send_dialog(message: Message):
     bot.send_message(message.chat.id, 'Салют!')
 
-
-# def apply_invert(img):
-#     photo = cv2.imread(src)
-#     return cv2.bitwise_not(photo)
 
 bot.polling(interval=0, timeout=10)
